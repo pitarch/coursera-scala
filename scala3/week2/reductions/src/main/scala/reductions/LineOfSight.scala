90367dbcdb356df33525d6f0292c5e67bd4f65b0
Albert Pitarch <albert.pitarch@axonix.com>
2020-07-07 09:00:49 +0100

    finish week 2

--- scala3/week2/reductions/src/main/scala/reductions/LineOfSight.scala
+++ scala3/week2/reductions/src/main/scala/reductions/LineOfSight.scala
@@ -9,7 +9,7 @@ object LineOfSightRunner {
     Key.exec.maxWarmupRuns -> 80,
     Key.exec.benchRuns -> 100,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val length = 10000000
@@ -41,49 +41,92 @@ case class Leaf(from: Int, until: Int, maxPrevious: Float) extends Tree
 object LineOfSight extends LineOfSightInterface {
 
   def lineOfSight(input: Array[Float], output: Array[Float]): Unit = {
-    ???
+    if (input.length > 0) output(0) = 0f
+    var i = 1
+    var max: Float = 0f
+    while (i < input.length) {
+      val tangent = input(i) / i
+      if (tangent > max) max = tangent
+      output(i) = max
+      i = i + 1
+    }
   }
 
   /** Traverses the specified part of the array and returns the maximum angle.
    */
   def upsweepSequential(input: Array[Float], from: Int, until: Int): Float = {
-    ???
+
+    var maxTangent = 0f
+    var i = from
+    while (i < until) {
+      val currentTangent: Float = if (i == 0) 0f else input(i) / i
+      if (currentTangent > maxTangent) maxTangent = currentTangent
+      i = i + 1
+    }
+    maxTangent
   }
 
   /** Traverses the part of the array starting at `from` and until `end`, and
-   *  returns the reduction tree for that part of the array.
+   * returns the reduction tree for that part of the array.
    *
-   *  The reduction tree is a `Leaf` if the length of the specified part of the
-   *  array is smaller or equal to `threshold`, and a `Node` otherwise.
-   *  If the specified part of the array is longer than `threshold`, then the
-   *  work is divided and done recursively in parallel.
+   * The reduction tree is a `Leaf` if the length of the specified part of the
+   * array is smaller or equal to `threshold`, and a `Node` otherwise.
+   * If the specified part of the array is longer than `threshold`, then the
+   * work is divided and done recursively in parallel.
    */
   def upsweep(input: Array[Float], from: Int, end: Int,
-    threshold: Int): Tree = {
-    ???
+              threshold: Int): Tree = {
+
+    if (end - from <= threshold) {
+      val max = upsweepSequential(input, from, end)
+//      println(s"upsweep Leaf: [$from, $end] => $max")
+      Leaf(from, end, max)
+    }
+    else {
+      val middle = from + (end - from) / 2
+//      println(s"parallel upsweep: left: [$from, $middle]  right: [$middle, $end]")
+      val (tL, tR) = parallel(upsweep(input, from, middle, threshold), upsweep(input, middle, end, threshold))
+      Node(tL, tR)
+    }
   }
 
   /** Traverses the part of the `input` array starting at `from` and until
-   *  `until`, and computes the maximum angle for each entry of the output array,
-   *  given the `startingAngle`.
+   * `until`, and computes the maximum angle for each entry of the output array,
+   * given the `startingAngle`.
    */
   def downsweepSequential(input: Array[Float], output: Array[Float],
-    startingAngle: Float, from: Int, until: Int): Unit = {
-    ???
+                          startingAngle: Float, from: Int, until: Int): Unit = {
+//    println(s"downsweepSequential: [$from, $until] => startingAngle: $startingAngle")
+    var i = from
+    var latest = startingAngle
+    while (i < until) {
+      val theAngle = if (i == 0) 0f else input(i) / i
+      latest = latest.max(theAngle)
+      output(i) = latest
+      i = i + 1
+    }
   }
 
   /** Pushes the maximum angle in the prefix of the array to each leaf of the
-   *  reduction `tree` in parallel, and then calls `downsweepSequential` to write
-   *  the `output` angles.
+   * reduction `tree` in parallel, and then calls `downsweepSequential` to write
+   * the `output` angles.
    */
   def downsweep(input: Array[Float], output: Array[Float], startingAngle: Float,
-    tree: Tree): Unit = {
-    ???
+                tree: Tree): Unit = {
+    tree match {
+      case Node(left, right) =>
+//        println(s"parallel downsweep: left max: ${left.maxPrevious}  right max: ${right.maxPrevious}")
+        parallel(downsweep(input, output, startingAngle.min(left.maxPrevious), left), downsweep(input, output, left.maxPrevious, right))
+      case Leaf(from, until, maxPrevious) =>
+//        println(s"downsweep/leaf: [$from, $until]/$maxPrevious")
+        downsweepSequential(input, output, startingAngle, from, until)
+    }
   }
 
   /** Compute the line-of-sight in parallel. */
-  def parLineOfSight(input: Array[Float], output: Array[Float],
-    threshold: Int): Unit = {
-    ???
+  def parLineOfSight(input: Array[Float], output: Array[Float], threshold: Int): Unit = {
+    val tree = upsweep(input, 0, input.length, threshold)
+    downsweep(input, output, 0f, tree)
+    output(0) = 0f
   }
 }
--- scala3/week2/reductions/src/test/scala/reductions/LineOfSightSuite.scala
+++ scala3/week2/reductions/src/test/scala/reductions/LineOfSightSuite.scala
@@ -4,11 +4,9 @@ import org.junit.Assert.assertEquals
 import org.junit._
 
 class LineOfSightSuite {
-  /*****************
-   * LINE OF SIGHT *
-   *****************/
 
   import LineOfSight._
+
   @Test def `lineOfSight should correctly handle an array of size 4`: Unit = {
     val output = new Array[Float](4)
     lineOfSight(Array[Float](0f, 1f, 8f, 9f), output)
@@ -16,107 +14,45 @@ class LineOfSightSuite {
   }
 
 
-
-
-  /*******************************
-   * PARALLEL COUNT CHANGE SUITE *
-   *******************************/
-
-  import ParallelCountChange._
-
-  @Test def `countChange should return 0 for money < 0`: Unit = {
-    def check(money: Int, coins: List[Int]) =
-      assert(countChange(money, coins) == 0,
-        s"countChang($money, _) should be 0")
-
-    check(-1, List())
-    check(-1, List(1, 2, 3))
-    check(-Int.MinValue, List())
-    check(-Int.MinValue, List(1, 2, 3))
+  @Test def `parLineOfSight should invoke the parallel construct 30 times (15 times during upsweep and 15 times during downsweep) for an array of size 17, with threshold 1`: Unit = {
+    val input = new Array[Float](17)
+    val output = new Array[Float](input.length)
+    parLineOfSight(input, output, 1)
   }
 
-  @Test def `countChange should return 1 when money == 0`: Unit = {
-    def check(coins: List[Int]) =
-      assert(countChange(0, coins) == 1,
-        s"countChang(0, _) should be 1")
-
-    check(List())
-    check(List(1, 2, 3))
-    check(List.range(1, 100))
+  @Test def `parLineOfSight should correctly compute the output for threshold 3`: Unit = {
+    val input = new Array[Float](6)
+    input(1) = 9f
+    val output = new Array[Float](input.length)
+    parLineOfSight(input, output, 3)
+    val expected = List(0.0, 9.0, 9.0, 9.0, 9.0, 9.0)
+    assertEquals(expected, output.toList)
   }
 
-  @Test def `countChange should return 0 for money > 0 and coins = List()`: Unit = {
-    def check(money: Int) =
-      assert(countChange(money, List()) == 0,
-        s"countChang($money, List()) should be 0")
-
-    check(1)
-    check(Int.MaxValue)
+  @Test def `parLineparLineOfSight should correctly compute the output for threshold 2`: Unit = {
+    val input = new Array[Float](5)
+    input(1) = 7f
+    input(2) = 2 * 7f
+    input(3) = 3 * 11f
+    input(4) = 4 * 12f
+    val output = new Array[Float](input.length)
+    parLineOfSight(input, output, 2)
+    val expected = List(0.0, 7.0, 7.0, 11.0, 12.0)
+    assertEquals(expected, output.toList)
   }
 
-  @Test def `countChange should work when there is only one coin`: Unit = {
-    def check(money: Int, coins: List[Int], expected: Int) =
-      assert(countChange(money, coins) == expected,
-        s"countChange($money, $coins) should be $expected")
-
-    check(1, List(1), 1)
-    check(2, List(1), 1)
-    check(1, List(2), 0)
-    check(Int.MaxValue, List(Int.MaxValue), 1)
-    check(Int.MaxValue - 1, List(Int.MaxValue), 0)
+  @Test def `downsweep should correctly handle trees with a single leaf`: Unit = {
+    val input = new Array[Float](5)
+    input(1) = 7f
+    input(2) = 2 * 7f
+    input(3) = 3 * 11f
+    input(4) = 4 * 12f
+    val output = new Array[Float](input.length)
+    parLineOfSight(input, output, input.length)
+    val expected = List(0.0, 7.0, 7.0, 11.0, 12.0)
+    assertEquals(expected, output.toList)
   }
 
-  @Test def `countChange should work for multi-coins`: Unit = {
-    def check(money: Int, coins: List[Int], expected: Int) = {
-      val result = countChange(money, coins)
-      assert(result == expected,
-        s"countChange($money, $coins) should be $expected but was $result")
-    }
-
-    check(4, List(1, 2), 3)
-    check(50, List(1, 2, 5, 10), 341)
-    check(250, List(1, 2, 5, 10, 20, 50), 177863)
-  }
-
-
-  /**********************************
-   * PARALLEL PARENTHESES BALANCING *
-   **********************************/
-
-  import ParallelParenthesesBalancing._
-
-  @Test def `balance should work for empty string`: Unit = {
-    def check(input: String, expected: Boolean) =
-      assert(balance(input.toArray) == expected,
-        s"balance($input) should be $expected")
-
-    check("", true)
-  }
-
-  @Test def `balance should work for string of length 1`: Unit = {
-    def check(input: String, expected: Boolean) =
-      assert(balance(input.toArray) == expected,
-        s"balance($input) should be $expected")
-
-    check("(", false)
-    check(")", false)
-    check(".", true)
-  }
-
-  @Test def `balance should work for string of length 2`: Unit = {
-    def check(input: String, expected: Boolean) =
-      assert(balance(input.toArray) == expected,
-        s"balance($input) should be $expected")
-
-    check("()", true)
-    check(")(", false)
-    check("((", false)
-    check("))", false)
-    check(".)", false)
-    check(".(", false)
-    check("(.", false)
-    check(").", false)
-  }
 
 
   @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
